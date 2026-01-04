394493b1c8c16ad98b9cda4ef86185e1040a790d
Jhonatan <jhonatan-ito@hotmail.com>
2026-01-05 03:57:10 +0900

    more stuff

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,8 +1,10 @@
 import DarkButton from "./DarkButton";
 import Tabs from "./Tabs";
 import logo from "../../../assets/dollar.png";
+import back from "../../../assets/back.png";
 import profile from "../../../assets/6681204.png";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
   const [displayMobile, setDisplayMobile] = useState("none");
@@ -25,6 +27,28 @@ export default function Header(props) {
         <div>
           <img className="profileContainer" src={profile} />
         </div>
+      </div>
+      <div style={{ display: displayMobile }} className="mobileMenu">
+        <Link to="/">
+          <div onClick={() => setDisplayMobile("none")} className="mobileTab">
+            dashboard
+          </div>
+        </Link>
+        <Link to="/simulation">
+          <div onClick={() => setDisplayMobile("none")} className="mobileTab">
+            simulation
+          </div>
+        </Link>
+        <Link to="/converter">
+          <div onClick={() => setDisplayMobile("none")} className="mobileTab">
+            USD/JPY
+          </div>
+        </Link>
+        <img
+          onClick={() => setDisplayMobile("none")}
+          className="backIcon"
+          src={back}
+        />
       </div>
     </div>
   );
